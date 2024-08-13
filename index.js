12f156b8e57e62bda329025ebfd478039b5a7f06
Behruz201004 <behruzbahtiyorov888@gmail.com>
2024-08-13 13:57:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+// let a = 'hello'
+
+// alert(typeof a);
+
+// if (5 > 4) {
+//     console.log('true');
+
+// }else {
+//     console.log('false');
+// }
+
+// for (let i = 1; i <=  10; i++) {
+//    console.log(i);
+
+// }
+
+// let a = prompt("Как тебя зовут");
+// let age = +prompt(" Скока тебе лет?");
+// let citiy = prompt(" Где ты живешь?");
+
+//  alert('Привет' + name + 'тебе' + age + 'лет, ты живешь в городе'  + сitiy )
+
+// alert(` вам   ${age} лет, ты живешь в городе  ${citiy}`);
+
+
+
+
+// const fn = (name) => {
+//     alert(`Привет  ${name} `)
+// }
+// fn('kvantum')
+
+
+// let user = {
+//     name: 'kvantum',
+//     surname:'null'   
+     
+//     cart: null
+// }
+
+// user.age = 13
+// delete user.name
+
+// console.log(user.surname);
+
+// let num1 = +prompt(' Введите первое число')
+// let num2= +prompt(' Введите  второе число')
+
+// if (num1 > num2) {
+//    alert(' первое число больше второе число')
+    
+// }else if (num1  >num2  ) {
+//     alert(' второе число  больше  первое число ')
+
+// }else{
+//     alert('они равны ')
+// }
+
+// let  namber = +prompt('Введите  число')
+
+// if(  namber == 0) {
+//      alert('Число равен нулю')
+// }else if (namber % 2 == 0  ) {
+//      alert('Число четное' )
+// }else{
+//     alert( 'Число не четное')  
+// }
+
+let secreNumber = Math.floor(Math.random() * 11)
+let guess = +parent(' Угадай число от 1 до 10')
+
+if (guess == secreNumber ) {
+    alert('Ты угадал! Позравляяю🤡😂😂😊') 
+}else{
+    alert('Не  угадал! Попробую ещо раз 😭😰😨🥶') 
+}
+
+alert(secreNumber)
